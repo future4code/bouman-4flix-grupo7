5c276c0e9f741442d62482d6c8a017a31f641101
unknown <eduardomendesnorberto@outlook.com>
2020-03-13 08:23:37 -0300

    Buscar filme pelo id implementado

---
+++ src/business/usecase/films/getFilmsById.ts
@@ -0,0 +1,34 @@
+import { FilmsDB } from "../../../data/filmsDB";
+
+export class GetFilmsByIdUC {
+    constructor(private db: FilmsDB) {}
+
+    public async execute(input: GetFilmsByIdUCInput): Promise<GetFilmsByIdUCOutput>{
+
+        const films = await this.db.getFilmsById(input.id);
+
+        return{
+            id: films.getId(),
+            title: films.getTitle(),
+            debut_date: films.getDebut_date(),
+            sinopse: films.getSinopse(),
+            link: films.getLink(),
+            length: films.getLength(),
+            image: films.getImage()
+        }
+    }
+}
+
+export interface GetFilmsByIdUCInput {
+    id: string;
+}
+
+export interface GetFilmsByIdUCOutput {
+    id: string;
+    title: string,
+    debut_date: Date,
+    sinopse: string,
+    link: string,
+    length: number,
+    image?: string
+}
